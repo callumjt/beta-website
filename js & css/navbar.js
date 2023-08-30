6e915db7205893779129520d30a2af1e4249e73c
callum <116295240+callumjt@users.noreply.github.com>
2023-08-30 22:03:38 +0100

    Add files via upload

--- js & css/navbar.js
+++ js & css/navbar.js
@@ -41,7 +41,7 @@ navbarButton2.classList = "button";
 navbarButton2.textContent = "Projects";
 const navbarButton2R = document.createElement("a");
 navbarButton2R.href = "/projects";
-if (window.location.pathname === "/projects" || window.location.pathname === "/projects.html") {
+if (window.location.pathname === "/projects" || window.location.pathname === "/projects/index.html") {
     navbarButton2.style.color = "#3b82f6";
     // navbarButton2.style.textShadow = "5px 0px 30px #001BFF";
 }
@@ -51,7 +51,7 @@ navbarButton3.classList = "button";
 navbarButton3.textContent = "Devlog";
 const navbarButton3R = document.createElement("a");
 navbarButton3R.href = "/devlogs";
-if (window.location.pathname === "/devlogs.html" || window.location.pathname === "/devlogs") {
+if (window.location.pathname === "/devlogs" || window.location.pathname === "/devlogs/index.html") {
     navbarButton3.style.color = "#3b82f6";
     // navbarButton3.style.textShadow = "5px 0px 30px #001BFF";
 }
@@ -71,6 +71,73 @@ navbarRight.appendChild(navbarButton3R);
 navbarButton3R.appendChild(navbarButton3);
 navbarRight.appendChild(lightToggleDiv);
 lightToggleDiv.appendChild(lightToggle);
+
+// mobile dropdown down here
+
+const navbarRightMobile = document.createElement('div')
+navbarRightMobile.classList = "rightMobile"
+
+const navbarRightMobileButton = document.createElement('div')
+navbarRightMobileButton.classList = "dropdownButton"
+
+const navbarRightMobileImg = document.createElement('img')
+navbarRightMobileImg.src = '/srcs/img/3\ lines.svg'
+
+navbarMain.appendChild(navbarRightMobile)
+navbarRightMobile.appendChild(navbarRightMobileButton)
+navbarRightMobileButton.appendChild(navbarRightMobileImg)
+
+const dropdown = document.createElement('div')
+dropdown.classList = "dropdown"
+dropdown.style.display = "none"
+
+const navbarButton11 = document.createElement("div");
+navbarButton11.classList = "button";
+navbarButton11.textContent = "Home";
+const navbarButton1R1 = document.createElement("a");
+navbarButton1R1.href = "/";
+if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
+    navbarButton11.style.color = "#3b82f6";
+    // navbarButton1.style.textShadow = "5px 0px 30px #001BFF";
+}
+
+const navbarButton21 = document.createElement("div");
+navbarButton21.classList = "button";
+navbarButton21.textContent = "Projects";
+const navbarButton2R1 = document.createElement("a");
+navbarButton2R1.href = "/projects";
+if (window.location.pathname === "/projects" || window.location.pathname === "/projects/index.html") {
+    navbarButton21.style.color = "#3b82f6";
+    // navbarButton2.style.textShadow = "5px 0px 30px #001BFF";
+}
+
+const navbarButton31 = document.createElement("div");
+navbarButton31.classList = "button";
+navbarButton31.textContent = "Devlog";
+const navbarButton3R1 = document.createElement("a");
+navbarButton3R1.href = "/devlogs";
+if (window.location.pathname === "/devlogs" || window.location.pathname === "/devlogs/index.html") {
+    navbarButton31.style.color = "#3b82f6";
+    // navbarButton3.style.textShadow = "5px 0px 30px #001BFF";
+}
+
+navbarMain.appendChild(dropdown)
+dropdown.appendChild(navbarButton1R1)
+navbarButton1R1.appendChild(navbarButton11)
+dropdown.appendChild(navbarButton2R1)
+navbarButton2R1.appendChild(navbarButton21)
+dropdown.appendChild(navbarButton3R1)
+navbarButton3R1.appendChild(navbarButton31)
+
+var dropdownState = false;
+
+if (window.innerWidth <= 630) {
+    navbarRight.style.display = "none"
+    navbarRightMobile.style.display = "inherit"
+} else {
+    navbarRight.style.display = "inherit"
+    navbarRightMobile.style.display = "none"
+}
 
 if (document.documentElement.scrollTop == 0) {
     // navbarMain.style.backgroundColor = "rgba(0, 0, 0, 0)";
@@ -93,3 +160,21 @@ document.addEventListener("scroll", function() {
 })
 
 // checks if top of page
+
+window.addEventListener('resize', function(a) {
+    if (window.innerWidth <= 630) {
+        navbarRight.style.display = "none"
+        navbarRightMobile.style.display = "inherit"
+    } else {
+        navbarRight.style.display = "inherit"
+        navbarRightMobile.style.display = "none"
+        navbarMain.style.height = "70px"
+        dropdownState = false
+    }
+})
+
+navbarRightMobile.addEventListener('click', function(a) {
+    dropdownState = dropdownState == true ? false : true
+    dropdown.style.display = dropdownState == true ? "inherit" : "none"
+    navbarMain.style.height = dropdownState == true ? '100%' : '70px'
+})
--- script.js
+++ script.js
@@ -6,17 +6,21 @@ var mode = localStorage.getItem("colorScheme");
 document.getElementsByTagName('style')[0].innerHTML = "@import url(/js\\ &\\ css/" + mode + ".css);";
 console.log(document.getElementsByTagName('style')[0].innerHTML)
 lightToggle.src = "/srcs/img/" + mode + ".svg"
+document.body.style.colorScheme = mode
 
 
 
 lightToggle.addEventListener("click", function() {
-if (mode === "dark") {
-    mode = "light";
-    localStorage.setItem("colorScheme", "light");
-} else {
-    mode = "dark";
-    localStorage.setItem("colorScheme", "dark");
-}
+// if (mode === "dark") {
+//     mode = "light";
+//     localStorage.setItem("colorScheme", "light");
+// } else {
+//     mode = "dark";
+//     localStorage.setItem("colorScheme", "dark");
+// }
+mode = mode == "dark" ? "light" : "dark"
+localStorage.setItem("colorScheme", mode)
+document.body.style.colorScheme = mode
 document.getElementsByTagName('style')[0].innerHTML = "@import url(/js\\ &\\ css/" + mode + ".css);";
 lightToggle.src = "/srcs/img/" + mode + ".svg"
 });
